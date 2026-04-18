b1cb6ece080769e18296c9404e6fc263a49001c4
ZeNoobi <ctaaseen@gmail.com>
2026-04-17 17:01:50 -0700

    Button Complete

--- app/components/modals/Modal.tsx
+++ app/components/modals/Modal.tsx
@@ -1,6 +1,7 @@
 'use client';
 import { useCallback, useEffect, useState } from "react";
 import { IoMdClose } from "react-icons/io";
+import Button from "../Button";
 
 interface ModalProps {
     isOpen?: boolean;
@@ -73,7 +74,7 @@ const Modal: React.FC<ModalProps> = ({ isOpen, onClose, onSubmit, title, body, f
                         {/*footer*/}
                         <div className="flex flex-col gap-2 p-6">
                             <div className="flex flex-row items-center gap-4 w-full">
-                                
+                                <Button label="My Button"/>
                             </div>
                         </div>
                     </div>
